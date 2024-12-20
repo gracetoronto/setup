f3d856654aa8e6ee6399625aa72db626f2d906a4
Justin Chiu <justin_chiu@outlook.com>
2024-12-20 00:36:45 -0500

    Create substitutes.js

---
+++ data/substitutes.js
@@ -0,0 +1,16 @@
+const substitutes = {
+   "XLR Cable": [
+      { item: "XLR Cable (Long)", quantity: 1 }
+   ],
+   "XLR Cable (Long)": [
+      { item: "XLR Cable", quantity: 2 }
+   ],
+   "DI Box": [
+      { item: "DI Box (Active)", quantity: 1 },
+      { item: "DI Box (Stereo)", quantity: 1 }
+   ],
+   "DI Box (Stereo)": [
+      { item: "DI Box", quantity: 2 },
+      { item: "DI Box (Active)", quantity: 2 }
+   ]
+}
